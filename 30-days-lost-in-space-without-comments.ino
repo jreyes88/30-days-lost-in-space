2a436c8f44b8ccc0f22852cd1a7114685c0db9d1
jjooeeyy <jreyes88@gmail.com>
2025-11-07 14:37:52 -0600

    Day 7: We still need to keep an eye on this

--- 30-days-lost-in-space-without-comments.ino
+++ 30-days-lost-in-space-without-comments.ino
@@ -2,36 +2,33 @@
 
 const byte PHOTORESISTOR_PIN = A0;
 
-const unsigned int MIN_DELAY = 50;
-const unsigned int MAX_DELAY = 500;
+const unsigned int BATTERY_CAPACITY = 50000;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PHOTORESISTOR_PIN, INPUT);
 
   Serial.begin(9600);
 }
 
+unsigned int battery_level = 0;
+
 void loop() {
-  unsigned int light_value = analogRead(PHOTORESISTOR_PIN);
-  Serial.print("Light value: ");
-  Serial.print(light_value);
-  static unsigned int darkest_light = light_value;
-  static unsigned int brightest_light = light_value;
-
-  if (light_value < darkest_light) {
-    darkest_light = light_value;
-  }
-  if (light_value > brightest_light) {
-    brightest_light = light_value;
+  if (battery_level < BATTERY_CAPACITY) {
+    battery_level += analogRead(PHOTORESISTOR_PIN);
+    if (battery_level > BATTERY_CAPACITY) {
+      battery_level = BATTERY_CAPACITY;
+    }
   }
 
-  unsigned int delay_value = map(light_value, darkest_light, brightest_light, MAX_DELAY, MIN_DELAY);
-  Serial.print(", Delay value: ");
-  Serial.println(delay_value);
+  printBatteryChargeLevel();
+  delay(100);
+}
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(delay_value);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(delay_value);
+void printBatteryChargeLevel() {
+  if (battery_level < BATTERY_CAPACITY) {
+    Serial.print(((double)battery_level / (double)BATTERY_CAPACITY) * 100);
+    Serial.println("%");
+  } else {
+    Serial.println("FULLY CHARGED");
+  }
 }
