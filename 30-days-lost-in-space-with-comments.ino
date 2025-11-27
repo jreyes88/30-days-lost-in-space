2eaf8f6db0b047f0a0188e7ea2fa277b7f6049a7
jjooeeyy <jreyes88@gmail.com>
2025-11-27 17:51:19 -0600

    Day 12: Can you hear us?

--- 30-days-lost-in-space-with-comments.ino
+++ 30-days-lost-in-space-with-comments.ino
@@ -1,16 +1,14 @@
 /*
  * 30 Days - Lost in Space
- * Day 11 - Starting your control panel
+ * Day 12 - Can you hear us?
  *
  * Learn more at https://learn.inventr.io/adventure
  *
- * Today we introduce you to a 4x4 button Keypad.  This keypad is very similar to
- * what is found on a calculator or even a digital phone.  This keypad uses only 8
- * pins to encode 16 buttons by dividing the keypad into rows and columns.  When
- * a button is pressed it can be identified by finding which row pin and which column
- * pin are pressed.
- *
- * We simplify this process by using a library written for decoding similar keypads.
+ * Today we will use the Keypad from Day 11 to control musical TONES using a
+ * small speaker.  To do this we will show how to take the characters returned
+ * by the Keypad library and convert them to musical notes played by our speaker.
+ * Tones will play until changed or stopped (by pressing the 'D' button at the
+ * bottom right of our 4x4 button matrix)
  *
  * Alex Eschenauer
  * David Schmidt
@@ -25,6 +23,7 @@
  *              can be accessed using an index.  Arrays can be one dimensional
  *              like a list (with one index) or two dimensional like a spreadsheet
  *              table with rows and columns (using row and column indexes).
+ * - for loop:  Used to repeately perform actions.
  *
  * Parts and electronics concepts introduced in this lesson.
  */
@@ -32,91 +31,79 @@
 // Explicitly include Arduino.h
 #include "Arduino.h"
 
-/*
- * Instead of writing all the code necessary to check all 8 pins and calculate
- * which button is pressed we will use a "library" that has already been written
- * for us and simply call functions in that library.
- *
- * Please refer to the lesson and lesson videos to load the Keypad library into
- * your Arduino IDE before you use it for the first time.
- *
- * In order to use the Keypad library we include the file "Keypad.h".
- */
+// Include Keypad library
 #include <Keypad.h>
 
-// Our keypad has 4 rows, each with 4 columns.
 const byte ROWS = 4;
 const byte COLS = 4;
 
-/*
- * A one dimensional array is like a list of similar variables.  We use an "index" number
- * to access each item.  To access values in an array we use the name of the array followed
- * immediately by the index in square brackets: name[index].  The index can either be a
- * constant ("name[3]") or a variable containing a value:
- *
- * byte my_index = 3;
- * name[my_index] = value;
- *
- * Indexes start with zero, not one.  Think of the index as a number *added* to the first
- * location.  So, to access the first item in our array we add 0.  To access the second
- * item in the array we add 1.
- *
- * In our first array below, ROW_PINS[0] is set to 5 and ROW_PINS[2] is 3.
- */
-
-// These constant arrays contain the pins connected to the keypad's row and column pins.
 const byte ROW_PINS[ROWS] = { 5, 4, 3, 2 };
 const byte COL_PINS[COLS] = { 6, 7, 8, 9 };
 
-/*
-  * Character Type (char)
-  *
-  * In Arduino C++ we have previousluy used strings of characters using double quotes ("my
-  * string").  But for a single character we need to use single quotes to indicate that there
-  * is just ONE character ('A').
-  */
-
-/*
- * Two dimensional array
- *
- * A two dimensional array combines multiple rows of one-dimensional array into a table
- * using two indexes using square brackets.  To access any item in the array the first
- * index is added to the row, and then the second index is added to the column.
- *
- * In the BUTTONS array below BUTTONS[0][0] is set to the character '1' and BUTTONS[2][3]
- * is set to the character 'C'.
- */
-
-// Since the keypad has 4 rows of 4 columns each we use a two dimensional array to
-// configure what character will be returned when each button is pressed.
-const char BUTTONS[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },  // Row 0
-  { '4', '5', '6', 'B' },  // Row 1
-  { '7', '8', '9', 'C' },  // Row 2
-  { '*', '0', '#', 'D' }   // Row 3
+char BUTTONS[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },  // 1st row
+  { '4', '5', '6', 'B' },  // 2nd row
+  { '7', '8', '9', 'C' },  // 3rd row
+  { '*', '0', '#', 'D' }   // 4th row
 };
 
-/*
- * Using the Keypad.h Library, we are going to define a custom keypad to return
- * the characters we wish when each key is pressed.
- *
- * To do this, we create a heroKeypad object configured with our BUTTONS array,
- * the pins used for the rows and columns and lastly how many rows and how many
- * columns are on our kepad.
- *
- * NOTE: this also does the proper pinMode() commands so we don't need them in our setup()
- */
-Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+Keypad myAwesomePad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+
+const unsigned int TONES[ROWS][COLS] = {
+  // a frequency tone for each button
+  { 31, 93, 147, 208 },
+  { 247, 311, 370, 440 },
+  { 523, 587, 698, 880 },
+  { 1397, 2637, 3729, 0 }  // Use frequency of 0 for bottom right key to end tone.
+};
+
+const byte BUZZER_PIN = 10;  // pin 10 drives the buzzer
 
 void setup() {
-  Serial.begin(9600);  // Initialize the serial monitor
+  Serial.begin(9600);  // Begin monitoring via the serial monitor
 }
 
-void loop() {
-  // heroKeypad.waitForKey() will wait here until any button is pressed and
-  // returns the character we defined in our BUTTONS array.
-  char pressedButton = heroKeypad.waitForKey();  // Wait until a button is pressed
 
-  // Display the character returned for the button that was pressed.
-  Serial.println(pressedButton);
+void loop() {
+  char button_character = myAwesomePad.waitForKey();  // Wait for a button to be pressed
+
+  /*
+   * For loops:
+   * for() has three parts.  Each is part is :
+   *              START - operations performed when loop starts
+   *              CONDITION - evaluate this condition as a boolean and stop loop
+   *                          when false
+   *              STEP - operations performed every time for loop ends
+   *
+   * NOTE: you will often see the short variable names "i", "j" and "k" used
+   *       for variables in the for statement.  Those particular letters are used
+   *       mostly for historical reasons going back to the FORTRAN language, but
+   *       the variables in loops tend to be short in order to keep the for command
+   *       readable.  The short names also remind you when used that they change
+   *       each time through the loop.
+   */
+  unsigned int tone_frequency = 0;  // Frequency to use for tone (default to 0, no tone)
+  for (byte i = 0; i < ROWS; i++) {
+    for (byte j = 0; j < COLS; j++) {
+      if (button_character == BUTTONS[i][j]) {  // found it, get the corresponding tone
+        tone_frequency = TONES[i][j];
+      }
+
+    }  // end j loop
+  }    // end i loop
+
+  Serial.print("Key: ");  //   send the button_character to serial monitor...
+  Serial.print(button_character);
+  Serial.print("   Freq: ");
+  Serial.println(tone_frequency);
+
+  /*
+   * The tone() function plays a tone until stopped.  The code continues to run as the tone plays.
+   */
+  if (tone_frequency > 0) {                // If tone frequency greater than 0...
+    tone(BUZZER_PIN, tone_frequency);  // ...then play the tone at that frequency until stopped
+  } else {
+    Serial.println("Stop tone");
+    noTone(BUZZER_PIN);  // Stop pressed (tone frequency of 0) so stop any tone playing
+  }
 }
--- 30-days-lost-in-space-without-comments.ino
+++ 30-days-lost-in-space-without-comments.ino
@@ -7,21 +7,50 @@ const byte COLS = 4;
 const byte ROW_PINS[ROWS] = { 5, 4, 3, 2 };
 const byte COL_PINS[COLS] = { 6, 7, 8, 9 };
 
-const char BUTTONS[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },  // Row 0
-  { '4', '5', '6', 'B' },  // Row 1
-  { '7', '8', '9', 'C' },  // Row 2
-  { '*', '0', '#', 'D' }   // Row 3
+char BUTTONS[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },  // 1st row
+  { '4', '5', '6', 'B' },  // 2nd row
+  { '7', '8', '9', 'C' },  // 3rd row
+  { '*', '0', '#', 'D' }   // 4th row
 };
 
-Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+Keypad myAwesomePad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+
+const unsigned int TONES[ROWS][COLS] = {
+  { 31, 93, 147, 208 },
+  { 247, 311, 370, 440 },
+  { 523, 587, 698, 880 },
+  { 1397, 2637, 3729, 0 }
+};
+
+const byte BUZZER_PIN = 10;
 
 void setup() {
-  Serial.begin(9600);  // Initialize the serial monitor
+  Serial.begin(9600);
 }
 
-void loop() {
-  char pressedButton = heroKeypad.waitForKey();
 
-  Serial.println(pressedButton);
+void loop() {
+  char button_character = myAwesomePad.waitForKey();
+  unsigned int tone_frequency = 0;
+  for (byte i = 0; i < ROWS; i++) {
+    for (byte j = 0; j < COLS; j++) {
+      if (button_character == BUTTONS[i][j]) {
+        tone_frequency = TONES[i][j];
+      }
+
+    }
+  }
+
+  Serial.print("Key: ");
+  Serial.print(button_character);
+  Serial.print("   Freq: ");
+  Serial.println(tone_frequency);
+
+  if (tone_frequency > 0) {                // If tone frequency greater than 0...
+    tone(BUZZER_PIN, tone_frequency);  // ...then play the tone at that frequency until stopped
+  } else {
+    Serial.println("Stop tone");
+    noTone(BUZZER_PIN);  // Stop pressed (tone frequency of 0) so stop any tone playing
+  }
 }
