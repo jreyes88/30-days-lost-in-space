1cae35a08d5e92793c4fe1bbbb777219de0a04a2
jjooeeyy <jreyes88@gmail.com>
2025-11-29 12:39:40 -0600

    Day 14: Advanced security panel

--- 30-days-lost-in-space-with-comments.ino
+++ 30-days-lost-in-space-with-comments.ino
@@ -1,12 +1,31 @@
 /*
  * 30 Days - Lost in Space
- * Day 13 - HERO Security 101
+ * Day 14 - Advanced Security Panel
  *
  * Learn more at https://learn.inventr.io/adventure
  *
- * Playing tones was fun, but now it's time to get back to business.  Let's use
- * the 4x4 button keypad input a Personal Identification Number (PIN) to unlock
- * access to the lander's control panel.
+ * Time to enhance our security system, taking it from functional to fabulous.  We'll
+ * add in our RGB LED to give us visual feedback and also expand the tones that play
+ * to give us audible feedback as you enter your PIN.
+ *
+ * IMPORTANT NOTE: The wiring for this sketch builds on the Day 13 wiring. But the
+ *                 addition of the RGB LED requires us to MOVE some of the Day 13
+ *                 wires.  This is because the RGB LED only works when connected to
+ *                 pins that support Pulse Width Modulation (PWM).  HERO pins that
+ *                 support PWM have a tilde ('~') preceeding their pin numbers.
+ *
+ *                 If you look at the Day 13 diagrams you see that we used PWM pins
+ *                 for the keypad and buzzer, leaving only one available.  Pins 12
+ *                 and 13 are still available so we'll move the wires on pins 9 and 10
+ *                 (PWM pins) to 12 and 13.
+ *
+ *                 Pin 13 is also used while the Arduino IDE is uploading sketches, and
+ *                 if you connect the buzzer to THAT pin you get noise every time you
+ *                 upload a new sketch (try it!).  So the buzzer will now be on pin 12
+ *                 and the keypad wire on pin 9 will move to pin 13.
+ *
+ *                 Because we used constants, we only need to make two small changes
+ *                 from the Day 13 sketch instead of hunting down and changing each use.
  *
  * Alex Eschenauer
  * David Schmidt
@@ -15,27 +34,25 @@
 
 /*
  * Arduino concepts introduced/documented in this lesson.
- * - return:  Used by functions to return a single value and return immediately
- *            to the caller of the function.
- * - boolean type: bool is used for a value that can be "true" or "false"
- *                 NOTE: when testing values for true/false the value 0 is considered
- *                       "false" and any non-zero value is considered "true".
+ * - complex logic with multiple devices
  *
  * Parts and electronics concepts introduced in this lesson.
+ * - Changing wiring when particular pins (PWM) are required for added components.
+ * - Tone() and PWM analogWrite() cannot both operate at the same time.
  */
 
 // Explicitly include Arduino.h
 #include "Arduino.h"
 
-// Include Keypad library#include <Keypad.h>
+// Include Keypad library
 #include <Keypad.h>
 
 // Our HERO keypad has 4 rows, each with 4 columns.
 const byte ROWS = 4;
 const byte COLS = 4;
 
-const byte PIN_LENGTH = 4;    // PIN code is 4 button presses
-char current_pin[PIN_LENGTH] = { '0', '0', '0', '0' }; // Initial PIN is four zeros.
+const byte PIN_LENGTH = 4;                           // PIN code is 4 button presses
+char password[PIN_LENGTH] = { '0', '0', '0', '0' };  // Initial password is four zeros.
 
 // Define what characters will be returned by each button
 const char BUTTONS[ROWS][COLS] = {
@@ -47,28 +64,37 @@ const char BUTTONS[ROWS][COLS] = {
 
 // Define row and column pins connected to the keypad
 const byte ROW_PINS[ROWS] = { 5, 4, 3, 2 };
-const byte COL_PINS[COLS] = { 6, 7, 8, 9 };
+const byte COL_PINS[COLS] = { 6, 7, 8, 13 };  // NOTE wire moved from Day 13's sketch to pin 13
 
+// Create our keypad object from the keypad configuration above
 Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
 
-const byte BUZZER_PIN = 10;  // pin 10 drives the buzzer
+const byte BUZZER_PIN = 12;  // NOTE that pin 12 drives the buzzer now
+
+const byte RED_PIN = 11;    // PWM pin controlling the red leg of our RGB LED
+const byte GREEN_PIN = 10;  // PWM pin ccontrolling the green leg of our RGB LED
+const byte BLUE_PIN = 9;    // PWM pin ccontrolling the blue leg of our RGB LED
 
 void setup() {
-  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  // Since we start out "locked", we initially display a red color.
+  displayColor(128, 0, 0);
 
   Serial.begin(9600);  // Begin monitoring via the serial monitor
-  delay(200);          // Delay a brief period to let things settle before displaying prompt.
-  Serial.println("Press * to enter new PIN or # to access the system.");
+  Serial.println("Press * to set new password or # to access the system.");
 }
 
 void loop() {
-  char button_character = heroKeypad.waitForKey();
+  char button_character = heroKeypad.getKey();  // Wait for a button press and save character
 
-  // Serial.println(button_character);
-  tone(BUZZER_PIN, 880, 100);
+  // The '#' button unlocks our console
+  if (button_character == '#') {          // Button to access system
+    giveInputFeedback();                  // Short beep with blue light
+    bool access_allowed = validatePIN();  // Have user enter PIN to allow access
 
-  if (button_character == '#') {  // button to access system
-    bool access_allowed = validatePIN();
     if (access_allowed) {
       Serial.println("Welcome, authorized user. You may now begin using the system.");
     } else {
@@ -77,57 +103,110 @@ void loop() {
     }
   }
 
-  if (button_character == '*') {  // button to change PIN
-    bool access_allowed = validatePIN();
+  // The '*' allows the user to enter a new PIN after entering current PIN for security
+  if (button_character == '*') {          // Button to change password
+    giveInputFeedback();                  // Short beep with blue light
+    bool access_allowed = validatePIN();  // Have user enter PIN to allow access
 
     if (access_allowed) {
-      Serial.println("Welcome, authorized user. Please Enter a new PIN: ");
+      displayColor(128, 80, 0);  // Display yellow LED while entering a NEW PIN
+      Serial.println("Welcome, authorized user. Please Enter a new password: ");
 
+      // Use a for() loop that runs once for each PIN character.  Each character read
+      // replaces a character in our original PIN.
       for (int i = 0; i < PIN_LENGTH; i++) {
-        button_character = heroKeypad.waitForKey();
-        tone(BUZZER_PIN, 880, 100);
+        password[i] = heroKeypad.waitForKey();  // replace PIN character with new character
+        // NOTE: After the LAST button press we will give Success feedback, but up to the
+        //       last character we simply give input feedback.  Since Array indices start
+        //       at 0, this if() statement will be true for all but the LAST character of
+        //       the new PIN.
+        if (i < (PIN_LENGTH - 1)) {
+          giveInputFeedback();
+          displayColor(128, 80, 0);  // override color
+        }
 
-        current_pin[i] = button_character;
         Serial.print("*");
       }
 
       Serial.println();  // add new line after last asterisk so next message is on next line
       Serial.println("PIN Successfully Changed!");
+      giveSuccessFeedback();  // TADA sound and green light for successful PIN change
     } else {
-      Serial.println("Access Denied. Cannot change PIN without the old or default.");
+      Serial.println("Access Denied. Cannot change PIN without entering current PIN first.");
       Serial.println("\nPress * to enter new PIN or # to access the system.");
     }
   }
 }
 
-/*
- * This function prompts the user to enter a PIN and returns true or false depending
- * on whether the PIN matches our saved PIN.
- *
- * NOTE: this function introduces the concept of a function that returns a single
- *       value.  First, we must indicate what type of value that will be returned
- *       in the initial function declaration.  The "bool" at the beginning of
- *       the declaration indicates that this function will return a "boolean" value
- *       of either true or false.
- */
-
+// Enter PIN and return false for bad PIN or true for good PIN
 bool validatePIN() {
   Serial.println("Enter PIN to continue.");
 
   for (int i = 0; i < PIN_LENGTH; i++) {
     char button_character = heroKeypad.waitForKey();
-    tone(BUZZER_PIN, 880, 100);
 
-    if (current_pin[i] != button_character) {
-      Serial.println();  // start next message on new line
+    if (password[i] != button_character) {
+      giveErrorFeedback();  // Error sound and red light
+      Serial.println();     // start next message on new line
       Serial.print("WRONG PIN DIGIT: ");
       Serial.println(button_character);
       return false;  // return false and exit function
     }
+    // Give normal input feedback for all but the LAST character
+    if (i < (PIN_LENGTH - 1)) {
+      giveInputFeedback();  // Short beep and blue LED
+    }
     Serial.print("*");
   }
 
-  Serial.println();  // add new line after last asterisk so next message is on next line
+  giveSuccessFeedback();  // PIN matched - TADA! sound with green LED
+  Serial.println();       // add new line after last asterisk so next message is on next line
   Serial.println("Device Successfully Unlocked!");
   return true;
+}
+
+// Display a color by providing Red, Green and Blue intensities (0-255)
+void displayColor(byte red_intensity, byte green_intensity, byte blue_intensity) {
+  analogWrite(RED_PIN, red_intensity);      // Set red LED intensity using PWM
+  analogWrite(GREEN_PIN, green_intensity);  // Set green LED intensity using PWM
+  analogWrite(BLUE_PIN, blue_intensity);    // Set blue LED intensity using PWM
+}
+
+/*
+ * NOTE:
+ * Both the PWM functions and tone() functions use some of the same HERO hardware
+ * for their functions.  If the RGB LED is on while a tone is played it will flicker
+ * or diplay other colors.  Because of this, we turn off the LED while a tone is
+ * being played and restore it immediately afterwards.
+ */
+
+// A recognized button was pressed.  Give short beep and blue LED
+void giveInputFeedback() {
+  displayColor(0, 0, 0);  // Turn off LED while playing tone
+  tone(BUZZER_PIN, 880, 200);
+  delay(200);               // Delay while tone is playing because tone() returns immediately
+  displayColor(0, 0, 128);  // Restore blue LED
+}
+
+// A matching PIN has been entered or a new PIN has been accepted.
+// Play TADA! sound and display green LED
+void giveSuccessFeedback() {
+  displayColor(0, 0, 0);  // Turn off LED while playing tone
+  tone(BUZZER_PIN, 300, 200);
+  delay(200);  // Delay while tone is playing because tone() returns immediately
+
+  tone(BUZZER_PIN, 500, 500);
+  delay(500);  // Delay while tone is playing because tone() returns immediately
+  displayColor(0, 128, 0); // Display green LED
+}
+
+// Bad PIN entered.  Play descending tone and display red LED
+void giveErrorFeedback() {
+  displayColor(0, 0, 0);  // Turn off LED while playing tone
+  tone(BUZZER_PIN, 300, 200);
+  delay(200);  // Delay while tone is playing because tone() returns immediately
+
+  tone(BUZZER_PIN, 200, 500);  // Frequency = 200Hz, Duration = 200ms
+  delay(500);  // Delay while tone is playing because tone() returns immediately
+  displayColor(128, 0, 0);  // Display red LED
 }
--- 30-days-lost-in-space-without-comments.ino
+++ 30-days-lost-in-space-without-comments.ino
@@ -5,7 +5,7 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 const byte PIN_LENGTH = 4;
-char current_pin[PIN_LENGTH] = { '0', '0', '0', '0' };
+char password[PIN_LENGTH] = { '0', '0', '0', '0' };
 
 const char BUTTONS[ROWS][COLS] = {
   { '1', '2', '3', 'A' },
@@ -15,27 +15,34 @@ const char BUTTONS[ROWS][COLS] = {
 };
 
 const byte ROW_PINS[ROWS] = { 5, 4, 3, 2 };
-const byte COL_PINS[COLS] = { 6, 7, 8, 9 };
+const byte COL_PINS[COLS] = { 6, 7, 8, 13 };
 
 Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
 
-const byte BUZZER_PIN = 10;
+const byte BUZZER_PIN = 12;
+
+const byte RED_PIN = 11;
+const byte GREEN_PIN = 10;
+const byte BLUE_PIN = 9;
 
 void setup() {
-  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  displayColor(128, 0, 0);
 
   Serial.begin(9600);
-  delay(200);
-  Serial.println("Press * to enter new PIN or # to access the system.");
+  Serial.println("Press * to set new password or # to access the system.");
 }
 
 void loop() {
-  char button_character = heroKeypad.waitForKey();
-
-  tone(BUZZER_PIN, 880, 100);
+  char button_character = heroKeypad.getKey();
 
   if (button_character == '#') {
+    giveInputFeedback();
     bool access_allowed = validatePIN();
+
     if (access_allowed) {
       Serial.println("Welcome, authorized user. You may now begin using the system.");
     } else {
@@ -45,23 +52,28 @@ void loop() {
   }
 
   if (button_character == '*') {
+    giveInputFeedback();
     bool access_allowed = validatePIN();
 
     if (access_allowed) {
-      Serial.println("Welcome, authorized user. Please Enter a new PIN: ");
+      displayColor(128, 80, 0);
+      Serial.println("Welcome, authorized user. Please Enter a new password: ");
 
       for (int i = 0; i < PIN_LENGTH; i++) {
-        button_character = heroKeypad.waitForKey();
-        tone(BUZZER_PIN, 880, 100);
+        password[i] = heroKeypad.waitForKey();
+        if (i < (PIN_LENGTH - 1)) {
+          giveInputFeedback();
+          displayColor(128, 80, 0);
+        }
 
-        current_pin[i] = button_character;
         Serial.print("*");
       }
 
       Serial.println();
       Serial.println("PIN Successfully Changed!");
+      giveSuccessFeedback();
     } else {
-      Serial.println("Access Denied. Cannot change PIN without the old or default.");
+      Serial.println("Access Denied. Cannot change PIN without entering current PIN first.");
       Serial.println("\nPress * to enter new PIN or # to access the system.");
     }
   }
@@ -72,18 +84,56 @@ bool validatePIN() {
 
   for (int i = 0; i < PIN_LENGTH; i++) {
     char button_character = heroKeypad.waitForKey();
-    tone(BUZZER_PIN, 880, 100);
 
-    if (current_pin[i] != button_character) {
+    if (password[i] != button_character) {
+      giveErrorFeedback();
       Serial.println();
       Serial.print("WRONG PIN DIGIT: ");
       Serial.println(button_character);
       return false;
     }
+    if (i < (PIN_LENGTH - 1)) {
+      giveInputFeedback();
+    }
     Serial.print("*");
   }
 
+  giveSuccessFeedback();
   Serial.println();
   Serial.println("Device Successfully Unlocked!");
   return true;
+}
+
+void displayColor(byte red_intensity, byte green_intensity, byte blue_intensity) {
+  analogWrite(RED_PIN, red_intensity);
+  analogWrite(GREEN_PIN, green_intensity);
+  analogWrite(BLUE_PIN, blue_intensity);
+}
+
+void giveInputFeedback() {
+  displayColor(0, 0, 0);
+  tone(BUZZER_PIN, 880, 200);
+  delay(200);
+  displayColor(0, 0, 128);
+}
+
+void giveSuccessFeedback() {
+  displayColor(0, 0, 0);
+  tone(BUZZER_PIN, 300, 200);
+  delay(200);
+
+  tone(BUZZER_PIN, 500, 500);
+  delay(500);
+  displayColor(0, 128, 0);
+}
+
+
+void giveErrorFeedback() {
+  displayColor(0, 0, 0);
+  tone(BUZZER_PIN, 300, 200);
+  delay(200);
+
+  tone(BUZZER_PIN, 200, 500);
+  delay(500);
+  displayColor(128, 0, 0);
 }
