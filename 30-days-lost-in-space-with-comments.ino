69533b405f880c5cd75a1607f565e2582747f484
jjooeeyy <jreyes88@gmail.com>
2025-12-13 10:07:23 -0600

    Day 28: Landing gear

--- 30-days-lost-in-space-with-comments.ino
+++ 30-days-lost-in-space-with-comments.ino
@@ -1,22 +1,20 @@
 /*
  * 30 Days - Lost in Space
- * Day 27 - Fun With Bitmaps, Part 2
+ * Day 28 - Landing Gear
  *
- * OK, now we know how to display bitmaps that cover the entire display,
- * but it would be very inefficient to pre-draw every possible display.
+ * Now that the safety of the mother ship is in sight we need to consider
+ * our final landing.  While the lander has landing gear, it (of course) was
+ * controlled from the failed computer.
  *
- * Instead, we can reduce the size of our bitmaps to whatever size is
- * desired and then display the partial bitmaps.
+ * We'll start by confirming all switches are in the OFF position and then
+ * enable our approach code.  Currently the approach code will only excersize
+ * the landing gear, but we'll build on that later to perform our approach
+ * and landing at the mother ship.
  *
- * In addition, we won't just be displaying all the bitmaps in a loop so
- * we need a way to *select* which bitmaps are being displayed.
- *
- * Let's use our DIP switches to display one of eight different bitmaps.
- * Since we only have three switches we'll read each as a bit in a binary
- * number.  This way three switches can give us eight distinct values.
- *
- * We'll also add our 4 digit 7 segment display to show which picture
- * is currently being displayed.
+ * We'll add your 16 button matrix to our control panel and use two
+ * of the buttons to raise and lower the landing gear.  You'll need
+ * confirmation of the gear position, so we'll use the OLED to display
+ * our gear status using bitmaps.
  *
  * Learn more at https://learn.inventr.io/adventure
  *
@@ -28,8 +26,10 @@
 /*
  * Arduino concepts introduced/documented in this lesson.
  * - partial bitmaps
+ * - using analog pins as digital pins
  * - switches to binary
  * - creating include files
+ * - enum with defined values
  *
  * Parts and electronics concepts introduced in this lesson.
  * -
@@ -61,20 +61,18 @@ U8G2_SH1106_128X64_NONAME_2_HW_I2C lander_display(U8G2_R0, /* reset=*/U8X8_PIN_N
  * click the tab to edit.  This lets us move all of our bitmaps into a separate
  * file so we don't have to scroll through them.
  */
-#include "switch_bitmaps.h"
+// ************************************************
+#include "landing_gear_bitmaps.h"
 
 // Create an array of pointers to each of the bitmap images.
 const static char* SWITCH_BITMAPS[] = {
-  SWITCHES_ZERO,
-  SWITCHES_ONE,
-  SWITCHES_TWO,
-  SWITCHES_THREE,
-  SWITCHES_FOUR,
-  SWITCHES_FIVE,
-  SWITCHES_SIX,
-  SWITCHES_SEVEN,
+  LANDING_1,
+  LANDING_2,
+  LANDING_3,
+  LANDING_4,
 };
 
+// ************************************************
 // Include file for 4 digit - 7 segment display library
 #include <TM1637Display.h>
 const byte BITMAP_NUMBER_DISPLAY_DIO_PIN = 2;
@@ -82,12 +80,50 @@ const byte BITMAP_NUMBER_DISPLAY_CLK_PIN = 3;
 // Construct bitmap_number_display handle.
 TM1637Display bitmap_number_display(BITMAP_NUMBER_DISPLAY_CLK_PIN, BITMAP_NUMBER_DISPLAY_DIO_PIN);
 
+// ************************************************
 // Define pins for our DIP switches
 // We will need almost all pins for Day 29, so here we show that we can use ANALOG
 // pins as digitial pins when necessary.
-const byte SWITCH_BIT_0_PIN = A2;  // switch for bit 0 of our 3 bit value
-const byte SWITCH_BIT_1_PIN = A1;  // switch for bit 1 of our 3 bit value
-const byte SWITCH_BIT_2_PIN = A0;  // switch for bit 2 of our 3 bit value
+const byte CONFIRM_LEVER_PIN = A2;  // switch for bit 0 of our 3 bit value
+const byte SYSTEMS_LEVER_PIN = A1;  // switch for bit 1 of our 3 bit value
+const byte THRUST_LEVER_PIN = A0;   // switch for bit 2 of our 3 bit value
+
+// ************************************************
+// Setup for our 4x4 button matrix.
+#include <Keypad.h>  // 4x4 button matrix keypad library
+const byte ROWS = 4;
+const byte COLS = 4;
+
+byte colPins[ROWS] = { 10, 11, 12, 13 };
+byte rowPins[COLS] = { 9, 8, 7, 6 };
+
+char buttons[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },  // 1st row
+  { '4', '5', '6', 'B' },  // 2nd row
+  { '7', '8', '9', 'C' },  // 3rd row
+  { '*', '0', '#', 'D' }   // 4th row
+};
+
+Keypad myAwesomePad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
+
+// ************************************************
+// Here are the states our code will run through.  We'll build on this later to
+// perform the actual approach flight and landing in the mother ship bay.
+enum APPROACH_STATE {
+  APPROACH_INIT,       // Ensure all switches are off to begin
+  APPROACH_PREFLIGHT,  //
+  // APPROACH_IN_FLIGHT,
+  APPROACH_FINAL,  // Lower landing gear!
+  // APPROACH_TOO_FAST,
+  // APPROACH_DOCKED
+};
+
+const int GEAR_BITMAP_COUNT = sizeof(SWITCH_BITMAPS) / sizeof(SWITCH_BITMAPS[0]);
+enum GEAR_STATE {
+  GEAR_IDLE = 0,
+  GEAR_LOWERING = 1,
+  GEAR_RAISING = -1
+};
 
 // ************************************************
 void setup(void) {
@@ -98,51 +134,165 @@ void setup(void) {
   bitmap_number_display.clear();           // Clear the display
 
   // Configure DIP switch pins
-  pinMode(SWITCH_BIT_0_PIN, INPUT);  // switch for bit 0 of our 3 bit value
-  pinMode(SWITCH_BIT_1_PIN, INPUT);  // switch for bit 1 of our 3 bit value
-  pinMode(SWITCH_BIT_2_PIN, INPUT);  // switch for bit 2 of our 3 bit value
+  pinMode(CONFIRM_LEVER_PIN, INPUT);  // switch for bit 0 of our 3 bit value
+  pinMode(SYSTEMS_LEVER_PIN, INPUT);  // switch for bit 1 of our 3 bit value
+  pinMode(THRUST_LEVER_PIN, INPUT);   // switch for bit 2 of our 3 bit value
 
-  lander_display.begin();  // initialize lander display
+  lander_display.begin();                     // initialize lander display
+  lander_display.setFont(u8g2_font_6x10_tr);  // Set text font
+  lander_display.setFontRefHeightText();      // Define how max text height is calculated
+  lander_display.setFontPosTop();             // Y coordinate for text is at top of tallest character
 }
 
 // ************************************************
 void loop(void) {
-  // Calculate our x and y offsets for our bitmap graphics
-  byte x_offset = (lander_display.getDisplayWidth() - BITMAP_WIDTH) / 2;
-  byte y_offset = (lander_display.getDisplayHeight() - BITMAP_HEIGHT) / 2;
+  static int current_gear_bitmap = 0;  // Start with image of lander with gear up
+  static enum APPROACH_STATE approach_state = APPROACH_INIT;
+  // APPROACH_FINAL; //
+  static enum GEAR_STATE gear_state = GEAR_IDLE;
+  static char last_key = -1;  // key previously seen
 
-  /*
-   * Now we build up a 3 bit binary number by reading each of our switches,
-   * ensuring that the read value is 1 for on and 0 for off (using the
-   * ternary notation), and then shifting each bit to it's desired location.
-   * This converts our three switches to the values 0-7:
-   *   0b00000000 = 0
-   *   0b00000001 = 1
-   *   0b00000010 = 2
-   *   0b00000011 = 3
-   *   0b00000100 = 4
-   *   0b00000101 = 5
-   *   0b00000110 = 6
-   *   0b00000011 = 7
-   */
+  // Read current values of all of our switches as booleans ("on" is true, "off" is false)
+  bool thrust_lever = digitalRead(THRUST_LEVER_PIN);
+  bool systems_lever = digitalRead(SYSTEMS_LEVER_PIN);
+  bool confirm_lever = digitalRead(CONFIRM_LEVER_PIN);
+  // Serial.println(approach_state);
+  // Serial.print("  Switches: ");
+  // Serial.print(thrust_lever);
+  // Serial.print(", ");
+  // Serial.print(systems_lever);
+  // Serial.print(", ");
+  // Serial.println(confirm_lever);
 
-  // Read bit 0 (0b00000001), ensure 0 or 1 and save.
-  byte switch_value = digitalRead(SWITCH_BIT_0_PIN) == HIGH ? 1 : 0;
-  // Read bit 1 (0b00000010), ensure 0 or 1, shift left 1 bit and OR it into current value.
-  switch_value |= (digitalRead(SWITCH_BIT_1_PIN) == HIGH ? 1 : 0) << 1;
-  // Read bit 2 (0b00000100), ensure 0 or 1, shift left 2 bits and OR it into current value.
-  switch_value |= (digitalRead(SWITCH_BIT_2_PIN) == HIGH ? 1 : 0) << 2;
+  switch (approach_state) {
+    case APPROACH_INIT:
+      // Serial.println("case APPROACH_INIT");
+      if (!thrust_lever && !systems_lever && !confirm_lever) {
+        approach_state = APPROACH_PREFLIGHT;
+      }
+      break;
+    case APPROACH_PREFLIGHT:
+      // Serial.println("case APPROACH_PREFLIGHT");
+      if (thrust_lever && systems_lever && confirm_lever) {
+        approach_state = APPROACH_FINAL;
+      }
+      break;
+    // case APPROACH_IN_FLIGHT:
+    //   Serial.println("case APPROACH_IN_FLIGHT");
+    //   approach_state = APPROACH_FINAL;
+    //   break;
+    case APPROACH_FINAL:
+      // Serial.println("case APPROACH_FINAL");
+      char customKey = myAwesomePad.getKey();
+      if (customKey && customKey != last_key) {
+        Serial.println(customKey);
+        last_key = customKey;
+      }
 
+      switch (customKey) {
+        case 'A':  // Lower landing gear unless already lowered
+          if (current_gear_bitmap != GEAR_BITMAP_COUNT - 1) {
+            gear_state = GEAR_LOWERING;
+          }
+          break;
+        case 'B':  // Raise landing gear unless already raised
+          if (current_gear_bitmap != 0) {
+            gear_state = GEAR_RAISING;
+          }
+      }
+      break;
+  }
+  // if (gear_state != GEAR_IDLE) {
+  current_gear_bitmap += gear_state;
+  if (current_gear_bitmap == 0 || current_gear_bitmap == GEAR_BITMAP_COUNT - 1) {
+    gear_state = GEAR_IDLE;
+  }
+
+  Serial.print("Gear: ");
+  Serial.println(current_gear_bitmap);
   // Display calculated switch value on our 4 digit display
-  bitmap_number_display.showNumberDecEx(switch_value);
+  // bitmap_number_display.showNumberDecEx(switch_value);
 
   // Display selected bitmap on our OLED lander display
   lander_display.firstPage();
   do {
-    // .drawXBMP() displays each bitmap centered in the display based
-    // on it's size.
-    lander_display.drawXBMP(x_offset, y_offset, BITMAP_WIDTH, BITMAP_HEIGHT, SWITCH_BITMAPS[switch_value]);
+    switch (approach_state) {
+      case APPROACH_INIT:
+      case APPROACH_PREFLIGHT:
+        preflightDisplay(approach_state, thrust_lever, systems_lever, confirm_lever);
+        break;
+      case APPROACH_FINAL:
+        finalDisplay(current_gear_bitmap);
+        break;
+    }
+
   } while (lander_display.nextPage());
 
   delay(100);
+}
+
+void preflightDisplay(enum APPROACH_STATE approach_state,
+                      bool thruster_lever,
+                      bool systems_lever,
+                      bool confirm_lever) {
+  lander_display.setFontPosTop();
+  byte y_offset = drawString(0, 0, "Exploration Lander");
+  y_offset = drawString(0, y_offset, "Approach Sequence");
+  // Set y_offset to point four lines above bottom of display
+  y_offset = lander_display.getDisplayHeight() - (4 * lander_display.getMaxCharHeight());
+  // Display last four lines
+  y_offset = drawString(0, y_offset,
+                        (String("Thrusters: ") + String(thruster_lever ? "ON" : "OFF")).c_str());
+  y_offset = drawString(0, y_offset,
+                        (String("Systems  : ") + String(systems_lever ? "ON" : "OFF")).c_str());
+  y_offset = drawString(0, y_offset,
+                        (String("Confirm  : ") + String(confirm_lever ? "ON" : "OFF")).c_str());
+
+  // Set y_offset to display text at bottom of display.
+  y_offset = lander_display.getDisplayHeight() - lander_display.getMaxCharHeight();
+  drawString(0, y_offset, (String("Countdown ") + liftoffStateToString(approach_state)).c_str());
+}
+
+void finalDisplay(int current_gear_bitmap) {
+  // Flash lander bitmap if gear isn't down
+  // display current gear bitmap as gear lowers
+
+  // .drawXBMP() displays each bitmap centered in the display based
+  // on it's size.
+
+  // Calculate our x and y offsets for our bitmap graphics
+  byte x_offset = (lander_display.getDisplayWidth() - LANDING_GEAR_BITMAP_WIDTH) / 2;
+  byte y_offset = (lander_display.getDisplayHeight() - LANDING_GEAR_BITMAP_HEIGHT) / 2;
+
+  lander_display.drawXBMP(x_offset, y_offset,
+                          LANDING_GEAR_BITMAP_WIDTH, LANDING_GEAR_BITMAP_HEIGHT,
+                          SWITCH_BITMAPS[current_gear_bitmap]);
+}
+
+// "helper" function that returns a different string for each enum state.
+String liftoffStateToString(enum APPROACH_STATE approach_state) {
+  switch (approach_state) {
+    case APPROACH_INIT:
+      return ("Init");
+      break;
+    case APPROACH_PREFLIGHT:
+      return ("Preflight");
+      break;
+      // case COUNTDOWN:
+      //   return ("Active");
+      //   break;
+      // case LIFTOFF:
+      //   return ("Complete");
+      //   break;
+      // case ABORT:
+      //   return ("ABORT");
+      //   break;
+  }
+}
+
+// Draw a line of text on our OLED display at x, y, returning new y
+// value that is immediately below the new line of text.
+byte drawString(byte x, byte y, char* string) {
+  lander_display.drawStr(x, y, string);
+  return (y + lander_display.getMaxCharHeight());  // return new y_offset on display
 }
--- 30-days-lost-in-space-without-comments.ino
+++ 30-days-lost-in-space-without-comments.ino
@@ -4,17 +4,13 @@
 
 U8G2_SH1106_128X64_NONAME_2_HW_I2C lander_display(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
-#include "switch_bitmaps.h"
+#include "landing_gear_bitmaps.h"
 
 const static char* SWITCH_BITMAPS[] = {
-  SWITCHES_ZERO,
-  SWITCHES_ONE,
-  SWITCHES_TWO,
-  SWITCHES_THREE,
-  SWITCHES_FOUR,
-  SWITCHES_FIVE,
-  SWITCHES_SIX,
-  SWITCHES_SEVEN,
+  LANDING_1,
+  LANDING_2,
+  LANDING_3,
+  LANDING_4,
 };
 
 #include <TM1637Display.h>
@@ -23,9 +19,41 @@ const byte BITMAP_NUMBER_DISPLAY_CLK_PIN = 3;
 
 TM1637Display bitmap_number_display(BITMAP_NUMBER_DISPLAY_CLK_PIN, BITMAP_NUMBER_DISPLAY_DIO_PIN);
 
-const byte SWITCH_BIT_0_PIN = A2;
-const byte SWITCH_BIT_1_PIN = A1;
-const byte SWITCH_BIT_2_PIN = A0;
+const byte CONFIRM_LEVER_PIN = A2;
+const byte SYSTEMS_LEVER_PIN = A1;
+const byte THRUST_LEVER_PIN = A0;
+
+#include <Keypad.h>
+const byte ROWS = 4;
+const byte COLS = 4;
+
+byte colPins[ROWS] = { 10, 11, 12, 13 };
+byte rowPins[COLS] = { 9, 8, 7, 6 };
+
+char buttons[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+Keypad myAwesomePad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
+
+enum APPROACH_STATE {
+  APPROACH_INIT,
+  APPROACH_PREFLIGHT,
+  // APPROACH_IN_FLIGHT,
+  APPROACH_FINAL,
+  // APPROACH_TOO_FAST,
+  // APPROACH_DOCKED
+};
+
+const int GEAR_BITMAP_COUNT = sizeof(SWITCH_BITMAPS) / sizeof(SWITCH_BITMAPS[0]);
+enum GEAR_STATE {
+  GEAR_IDLE = 0,
+  GEAR_LOWERING = 1,
+  GEAR_RAISING = -1
+};
 
 void setup(void) {
   Serial.begin(9600);
@@ -33,27 +61,148 @@ void setup(void) {
   bitmap_number_display.setBrightness(7);
   bitmap_number_display.clear();
 
-  pinMode(SWITCH_BIT_0_PIN, INPUT);
-  pinMode(SWITCH_BIT_1_PIN, INPUT);
-  pinMode(SWITCH_BIT_2_PIN, INPUT);
+  pinMode(CONFIRM_LEVER_PIN, INPUT);
+  pinMode(SYSTEMS_LEVER_PIN, INPUT);
+  pinMode(THRUST_LEVER_PIN, INPUT);
 
   lander_display.begin();
+  lander_display.setFont(u8g2_font_6x10_tr);
+  lander_display.setFontRefHeightText();
+  lander_display.setFontPosTop();
 }
 
 void loop(void) {
-  byte x_offset = (lander_display.getDisplayWidth() - BITMAP_WIDTH) / 2;
-  byte y_offset = (lander_display.getDisplayHeight() - BITMAP_HEIGHT) / 2;
+  static int current_gear_bitmap = 0;
+  static enum APPROACH_STATE approach_state = APPROACH_INIT;
 
-  byte switch_value = digitalRead(SWITCH_BIT_0_PIN) == HIGH ? 1 : 0;
-  switch_value |= (digitalRead(SWITCH_BIT_1_PIN) == HIGH ? 1 : 0) << 1;
-  switch_value |= (digitalRead(SWITCH_BIT_2_PIN) == HIGH ? 1 : 0) << 2;
+  static enum GEAR_STATE gear_state = GEAR_IDLE;
+  static char last_key = -1;
 
-  bitmap_number_display.showNumberDecEx(switch_value);
+  bool thrust_lever = digitalRead(THRUST_LEVER_PIN);
+  bool systems_lever = digitalRead(SYSTEMS_LEVER_PIN);
+  bool confirm_lever = digitalRead(CONFIRM_LEVER_PIN);
+  // Serial.println(approach_state);
+  // Serial.print("  Switches: ");
+  // Serial.print(thrust_lever);
+  // Serial.print(", ");
+  // Serial.print(systems_lever);
+  // Serial.print(", ");
+  // Serial.println(confirm_lever);
+
+  switch (approach_state) {
+    case APPROACH_INIT:
+      // Serial.println("case APPROACH_INIT");
+      if (!thrust_lever && !systems_lever && !confirm_lever) {
+        approach_state = APPROACH_PREFLIGHT;
+      }
+      break;
+    case APPROACH_PREFLIGHT:
+      // Serial.println("case APPROACH_PREFLIGHT");
+      if (thrust_lever && systems_lever && confirm_lever) {
+        approach_state = APPROACH_FINAL;
+      }
+      break;
+    // case APPROACH_IN_FLIGHT:
+    //   Serial.println("case APPROACH_IN_FLIGHT");
+    //   approach_state = APPROACH_FINAL;
+    //   break;
+    case APPROACH_FINAL:
+      // Serial.println("case APPROACH_FINAL");
+      char customKey = myAwesomePad.getKey();
+      if (customKey && customKey != last_key) {
+        Serial.println(customKey);
+        last_key = customKey;
+      }
+
+      switch (customKey) {
+        case 'A':
+          if (current_gear_bitmap != GEAR_BITMAP_COUNT - 1) {
+            gear_state = GEAR_LOWERING;
+          }
+          break;
+        case 'B':
+          if (current_gear_bitmap != 0) {
+            gear_state = GEAR_RAISING;
+          }
+      }
+      break;
+  }
+
+  current_gear_bitmap += gear_state;
+  if (current_gear_bitmap == 0 || current_gear_bitmap == GEAR_BITMAP_COUNT - 1) {
+    gear_state = GEAR_IDLE;
+  }
+
+  Serial.print("Gear: ");
+  Serial.println(current_gear_bitmap);
+  // bitmap_number_display.showNumberDecEx(switch_value);
 
   lander_display.firstPage();
   do {
-    lander_display.drawXBMP(x_offset, y_offset, BITMAP_WIDTH, BITMAP_HEIGHT, SWITCH_BITMAPS[switch_value]);
+    switch (approach_state) {
+      case APPROACH_INIT:
+      case APPROACH_PREFLIGHT:
+        preflightDisplay(approach_state, thrust_lever, systems_lever, confirm_lever);
+        break;
+      case APPROACH_FINAL:
+        finalDisplay(current_gear_bitmap);
+        break;
+    }
+
   } while (lander_display.nextPage());
 
   delay(100);
+}
+
+void preflightDisplay(enum APPROACH_STATE approach_state,
+                      bool thruster_lever,
+                      bool systems_lever,
+                      bool confirm_lever) {
+  lander_display.setFontPosTop();
+  byte y_offset = drawString(0, 0, "Exploration Lander");
+  y_offset = drawString(0, y_offset, "Approach Sequence");
+  y_offset = lander_display.getDisplayHeight() - (4 * lander_display.getMaxCharHeight());
+  y_offset = drawString(0, y_offset,
+                        (String("Thrusters: ") + String(thruster_lever ? "ON" : "OFF")).c_str());
+  y_offset = drawString(0, y_offset,
+                        (String("Systems  : ") + String(systems_lever ? "ON" : "OFF")).c_str());
+  y_offset = drawString(0, y_offset,
+                        (String("Confirm  : ") + String(confirm_lever ? "ON" : "OFF")).c_str());
+
+  y_offset = lander_display.getDisplayHeight() - lander_display.getMaxCharHeight();
+  drawString(0, y_offset, (String("Countdown ") + liftoffStateToString(approach_state)).c_str());
+}
+
+void finalDisplay(int current_gear_bitmap) {
+  byte x_offset = (lander_display.getDisplayWidth() - LANDING_GEAR_BITMAP_WIDTH) / 2;
+  byte y_offset = (lander_display.getDisplayHeight() - LANDING_GEAR_BITMAP_HEIGHT) / 2;
+
+  lander_display.drawXBMP(x_offset, y_offset,
+                          LANDING_GEAR_BITMAP_WIDTH, LANDING_GEAR_BITMAP_HEIGHT,
+                          SWITCH_BITMAPS[current_gear_bitmap]);
+}
+
+String liftoffStateToString(enum APPROACH_STATE approach_state) {
+  switch (approach_state) {
+    case APPROACH_INIT:
+      return ("Init");
+      break;
+    case APPROACH_PREFLIGHT:
+      return ("Preflight");
+      break;
+      // case COUNTDOWN:
+      //   return ("Active");
+      //   break;
+      // case LIFTOFF:
+      //   return ("Complete");
+      //   break;
+      // case ABORT:
+      //   return ("ABORT");
+      //   break;
+  }
+}
+
+byte drawString(byte x, byte y, char* string) {
+  lander_display.drawStr(x, y, string);
+  return (y + lander_display.getMaxCharHeight());
 }
